4f3cf40d85c6bfbc7959332d968790c1224dbd5c
macarenaEntelgy <macarena.rica@entelgy.com>
2024-10-16 15:57:34 +0000

    nuevos cambios

--- webapp/controller/MainView.controller.js
+++ webapp/controller/MainView.controller.js
@@ -1,12 +1,19 @@
 sap.ui.define([
     "sap/ui/core/mvc/Controller"
 ],
+/**
+ * @param {typeof sap.ui.core.mvc.Controller} Controller 
+ */
 function (Controller) {
     "use strict";
 
     return Controller.extend("dev.invoices.controller.MainView", {
-        onInit: function () {
 
+        onInit: function () {
+            const oJSONModel = new sap.ui.model.json.JSONModel();
+            const oView = this.getView();
+            oJSONModel.loadData("./model/pantallaDeSeleccion.json");
+            oView.setModel(oJSONModel, "pantallaDeSeleccion");
         }
     });
 });
