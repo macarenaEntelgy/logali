a874a81be5e462672a1e86177c53adec1555fda9
macarenaEntelgy <macarena.rica@entelgy.com>
2024-10-16 19:20:10 +0000

    nuevos cambios

--- webapp/controller/MainView.controller.js
+++ webapp/controller/MainView.controller.js
@@ -12,8 +12,18 @@ function (Controller) {
         onInit: function () {
             const oJSONModel = new sap.ui.model.json.JSONModel();
             const oView = this.getView();
-            oJSONModel.loadData("./model/pantallaDeSeleccion.json");
-            oView.setModel(oJSONModel, "pantallaDeSeleccion");
+            oJSONModel.loadData("./model/SelectionScreenMenu.json");
+            oView.setModel(oJSONModel, "selectionScreen");
+        },
+
+        onFilter: function (oEvent) {
+
+        },
+
+        onClearFilter: function() {
+           const oModelSelScreen = this.getView().getModel("selectionScreen");
+           oModelSelScreen.setProperty("/ShipName", "");
+           oModelSelScreen.setProperty("/CountryKey", "");
         }
     });
 });
